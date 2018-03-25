cc6d92a6bf22cb2543cb04a99ce3b63bd0dedf3b
Zac <yourspace.zh@gmail.com>
2018-03-26 00:03:24 +1030

    Formatting and Comments
    Fix indentation on List and Setup Manager
    Add missing comments and TODOs to files

--- src/lib/ListManager.class.php
+++ src/lib/ListManager.class.php
@@ -1,50 +1,78 @@
 <?php
 
-class ListManager {
-
-  private $lists = array();
-
-  public function getLists()
+  class ListManager
   {
-    return $this-lists;
-  }
 
-  public function setLists($lists = array())
-  {
-    if (!is_array($lists)) {
-      throw new InvalidArgumentException('Argument must be an array');
+    private $lists = array();
+
+    /**
+     * @return array
+     */
+    public function getLists()
+    {
+      return $this->lists;
     }
 
-    $this->lists = $lists;
+    /**
+     * @param array $lists
+     *
+     * @return $this
+     */
+    public function setLists($lists = array())
+    {
+      if (!is_array($lists)) {
+        throw new InvalidArgumentException('Argument must be an array');
+      }
 
-    return $this;
+      $this->lists = $lists;
+
+      return $this;
+    }
+
+    /**
+     * @param null  $name
+     * @param array $default
+     *
+     * @return $this
+     */
+    public function getListByName($name = null, $default = [])
+    {
+      // TODO
+      return $this;
+    }
+
+    /**
+     * Note that this function doesn't read the contents of the file, it just adds the filepath to a collection for us
+     * to use later
+     *
+     * @param null $filename
+     *
+     * @return $this
+     */
+    public function addListFromFile($filename = null)
+    {
+      // TODO - handle local filenames and urls
+      // FileManger::readContents()
+
+      return $this;
+    }
+
+    /**
+     *
+     */
+    private function deduplicate()
+    {
+
+    }
+
+    /**
+     *
+     */
+    private function combineLists()
+    {
+
+    }
+
+    // NOTE : Something needs to be done in order to make each list manageable
+    //  and handle duplicate lists, and duplicate list names
   }
-
-
-  public function getListByName($name = null, $default = [])
-  {
-    // TODO
-    return $this;
-  }
-
-  public function addListFromFile($filename = null)
-  {
-    // TODO - handle local filenames and urls
-    // FileManger::readContents()
-
-    return $this;
-  }
-
-  public function deduplicate()
-  {
-
-  }
-
-  public function combineLists()
-  {
-
-  }
-
-  // NOTE : Something needs to be done in order to make each list manageable
-  //  and handle duplicate lists, and duplicate list names
-}
--- src/lib/SetupManager.class.php
+++ src/lib/SetupManager.class.php
@@ -1,70 +1,71 @@
 <?php
 
-class SetupManager {
-
-
-  /**
-  * Checks the setup of the project and creates any missing directories
-  * Returns true on successfully setup project, false if otherwise
-  *
-  * @param bool $create_missing Create missing directories and files
-  *
-  * @return bool
-  */
-  public static function checkProject($create_missing = true)
+  class SetupManager
   {
-    if (FileManager::getRootDirectory() === null) {
-      throw new Exception('Unable to get root directory, something is terribly wrong (getRootDirectory returned null)');
-    }
 
-    $errors = array();
-    $directories_needed = array(
-      FileManager::getTempDirectory(),
-      FileManager::getOutputDirectory(),
-    );
+    /**
+     * Checks the setup of the project and creates any missing directories
+     * Returns true on successfully setup project, false if otherwise
+     *
+     * @param bool $create_missing Create missing directories and files
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public static function checkProject($create_missing = true)
+    {
+      if (FileManager::getRootDirectory() === null) {
+        throw new Exception('Unable to get root directory, something is terribly wrong (getRootDirectory returned null)');
+      }
 
-    foreach ($directories_needed as $directory_needed) {
-      if (!file_exists($directory_needed)) {
-        echo 'Directory \'' . $directory_needed . '\' does not exist ' . PHP_EOL;
-        if ($create_missing) {
-          echo 'Attempting to create directory \'' . $directory_needed . '\'' . PHP_EOL;
-          if (!mkdir($directory_needed)) {
-            $errors[$directory_needed . '_create'] = 'Unable to make directory ' . $directory_needed;
+      $errors             = array();
+      $directories_needed = array(
+        FileManager::getTempDirectory(),
+        FileManager::getOutputDirectory(),
+      );
+
+      foreach ($directories_needed as $directory_needed) {
+        if (!file_exists($directory_needed)) {
+          echo 'Directory \'' . $directory_needed . '\' does not exist ' . PHP_EOL;
+          if ($create_missing) {
+            echo 'Attempting to create directory \'' . $directory_needed . '\'' . PHP_EOL;
+            if (!mkdir($directory_needed)) {
+              $errors[$directory_needed . '_create'] = 'Unable to make directory ' . $directory_needed;
+            }
+            //TODO add fixing of mode for directory
+
+          } else {
+            $errors[$directory_needed . '_create'] = 'Directory ' . $directory_needed . ' does not exists, $create_missing is false';
           }
-          //TODO add fixing of mode for directory
-
-        } else {
-          $errors[$directory_needed . '_create'] = 'Directory ' . $directory_needed . ' does not exists, $create_missing is false';
         }
       }
-    }
 
-    // Check for the required files
-    $required_files = array(
-      FileManager::BLACKLIST_FILE,
-      FileManager::BLACKLIST_MANUAL_ENTRIES_FILE,
-      FileManager::WHITELIST_MANUAL_ENTRIES_FILE,
-    );
+      // Check for the required files
+      $required_files = array(
+        FileManager::BLACKLIST_FILE,
+        FileManager::BLACKLIST_MANUAL_ENTRIES_FILE,
+        FileManager::WHITELIST_MANUAL_ENTRIES_FILE,
+      );
 
-    foreach ($required_files as $required_file) {
-      $filename = FileManager::getRootDirectory() . DIRECTORY_SEPARATOR . $required_file;
-      if (!is_readable($filename)) {
-        echo $filename . ' is not readable, checking if it exists';
-        $status = 'File exists, check read permissions on file';
-        if (!file_exists($filename)) {
-          $status = 'File does not exist. Creating empty file';
-          touch($filename);
+      foreach ($required_files as $required_file) {
+        $filename = FileManager::getRootDirectory() . DIRECTORY_SEPARATOR . $required_file;
+        if (!is_readable($filename)) {
+          echo $filename . ' is not readable, checking if it exists';
+          $status = 'File exists, check read permissions on file';
+          if (!file_exists($filename)) {
+            $status = 'File does not exist. Creating empty file';
+            touch($filename);
+          }
+
         }
-
       }
+
+      if (!empty($errors)) {
+        //TODO outputting of errors
+        return false;
+      }
+
+      return true;
     }
 
-    if (!empty($errors)) {
-      //TODO outputting of errors
-      return false;
-    }
-
-    return true;
   }
-
-}
